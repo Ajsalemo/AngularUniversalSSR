0d2571951bcf8344ace38104abe9d27a0f5789db
Anthony Salemo <ajssalemo@gmail.com>
2021-07-17 17:29:02 -0400

    added path aliases for components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './components/app-root/app.component';
+import { AppComponent } from '@components/app-root/app.component';
 
 @NgModule({
   declarations: [AppComponent],
