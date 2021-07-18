0a66678982f9795d96ec4c28172629f115ef90d6
Anthony Salemo <ajssalemo@gmail.com>
2021-07-18 18:37:41 -0400

    integrated tailwindcss, starting on scaffolding the general UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from '@components/app-root/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { SideNavComponent } from '@components/app-root/side-nav/side-nav.component';
+import { SideNavComponent } from '@components/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [AppComponent, SideNavComponent],
