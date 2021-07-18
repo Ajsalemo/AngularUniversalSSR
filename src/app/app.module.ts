2aec996928569b0920e8ead57a8f84f2508f7f2c
Anthony Salemo <ajssalemo@gmail.com>
2021-07-18 17:47:07 -0400

    added tailwindcss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from '@components/app-root/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { SideNavComponent } from '@components/app-root/side-nav/side-nav.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SideNavComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
 @NgModule({
-  imports: [MatSliderModule],
-  exports: [MatSliderModule],
+  imports: [MatSliderModule, MatSidenavModule],
+  exports: [MatSliderModule, MatSidenavModule],
 })
 export class MaterialModule {}
-    
