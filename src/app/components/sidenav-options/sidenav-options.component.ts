3fac282c6579fd9747eacdba96f06ddecd7a496b
Anthony Salemo <ajssalemo@gmail.com>
2021-07-19 22:30:01 -0400

    updated the component array to include a property for aria-labels

--- src/app/components/sidenav-options/sidenav-options.component.ts
+++ src/app/components/sidenav-options/sidenav-options.component.ts
@@ -8,11 +8,36 @@ export class SidenavOptionsComponent implements OnInit {
   constructor() {}
 
   sidenavOptionLinks: any[] = [
-    { text: 'My Day', link: 'TBD' },
-    { text: 'Important', link: 'TBD' },
-    { text: 'Planned', link: 'TBD' },
-    { text: 'Assigned To You', link: 'TBD' },
-    { text: 'Tasks', link: 'TBD' }
+    {
+      text: 'My Day',
+      link: 'TBD',
+      icon: 'flare',
+      ariaLabelText: 'My Day icon',
+    },
+    {
+      text: 'Important',
+      link: 'TBD',
+      icon: 'star_border',
+      ariaLabelText: 'Important Tasks icon',
+    },
+    {
+      text: 'Planned',
+      link: 'TBD',
+      icon: 'flare',
+      ariaLabelText: 'Planned Tasks icon',
+    },
+    {
+      text: 'Assigned To You',
+      link: 'TBD',
+      icon: 'flare',
+      ariaLabelText: 'Assigned Tasks icon',
+    },
+    {
+      text: 'Tasks',
+      link: 'TBD',
+      icon: 'flare',
+      ariaLabelText: 'Normal Tasks icon',
+    },
   ];
 
   ngOnInit(): void {}
