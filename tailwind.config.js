6cd1ccda2db6652ba3ea905ac261b194aff311da
Anthony Salemo <ajssalemo@gmail.com>
2021-07-20 20:29:39 -0400

    corrected the border break in the sidenav pane

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,9 @@ module.exports = {
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      borderWidth: ["hover"],
+    },
   },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
