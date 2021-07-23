2ed45b2d0d494db44dcc487294f2cdc15947c3e7
Anthony Salemo <ajssalemo@gmail.com>
2021-07-23 18:57:05 -0400

    adding a theme menu for different background options

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
 @NgModule({
-  imports: [MatSidenavModule, MatIconModule],
-  exports: [MatSidenavModule, MatIconModule],
+  imports: [MatSidenavModule, MatIconModule, MatMenuModule],
+  exports: [MatSidenavModule, MatIconModule, MatMenuModule],
 })
 export class MaterialModule {}
