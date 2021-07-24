23de8015338738e771316faeb27bc6b305938d23
Anthony Salemo <ajssalemo@gmail.com>
2021-07-24 15:34:36 -0400

    corrected the theme menu layout. displaying available background colors to choose from

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 @NgModule({
   imports: [MatSidenavModule, MatIconModule, MatMenuModule],
   exports: [MatSidenavModule, MatIconModule, MatMenuModule],
