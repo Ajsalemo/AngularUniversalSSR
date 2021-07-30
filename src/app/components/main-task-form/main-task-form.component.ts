19f911f9fdf40f46a9194ac1a3666113e7f79340
Anthony Salemo <ajssalemo@gmail.com>
2021-07-30 19:35:00 -0400

    added some form validation

--- src/app/components/main-task-form/main-task-form.component.ts
+++ src/app/components/main-task-form/main-task-form.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-main-task-form',
   templateUrl: './main-task-form.component.html',
 })
 export class MainTaskFormComponent implements OnInit {
-  task: string = '';
   constructor() {}
 
   mainTaskForm = new FormGroup({
-    task: new FormControl('', [
-      Validators.required,
-      Validators.minLength(1)
-    ]),
+    task: new FormControl('', [Validators.required, Validators.minLength(1)]),
   });
+
+  // Field getter
+  get getTaskControl() {
+    return this.mainTaskForm.controls;
+  }
 
   submitMainTaskForm(data: any) {
     console.log(data.value.task);
