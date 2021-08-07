31bc73b5f78c71f18d25d4307299f14e7ec6f3c8
Anthony Salemo <ajssalemo@gmail.com>
2021-08-07 14:15:04 -0400

    able to programatically change icons for tasks on-hover

--- src/app/components/main-task-form/main-task-form.component.ts
+++ src/app/components/main-task-form/main-task-form.component.ts
@@ -8,6 +8,7 @@ import { FormServicesService } from '@services/formServices/form-services.servic
 })
 export class MainTaskFormComponent implements OnInit {
   mainTasksToDisplay: any;
+  changeTaskIconOnHover: boolean = false;
   constructor(private formServicesService: FormServicesService) {}
 
   mainTaskForm = new FormGroup({
@@ -19,13 +20,21 @@ export class MainTaskFormComponent implements OnInit {
     return this.mainTaskForm.controls;
   }
 
+  changeTaskIconTernary(): string {
+    if (this.changeTaskIconOnHover === true) {
+      return 'check_circle';
+    } else {
+      return 'check_circle_outline';
+    }
+  }
+
   // Function to submit tasks on the main form/input
   submitMainTaskForm(data: any) {
     this.formServicesService
       .mainTaskFormSubmitTodo(data.value)
       .then((res: any) => {
         // Reset the form values after submission
-        this.mainTaskForm.reset()
+        this.mainTaskForm.reset();
         this.formServicesService.mainTaskFormGetAllTodos().then((res: any) => {
           console.log(res);
           this.mainTasksToDisplay = res;
