35a25fb0ea8fcda2fc0c1fb30ef30893913cc4fa
Anthony Salemo <ajssalemo@gmail.com>
2021-08-26 19:12:27 -0400

    able to successfully authenticate logins

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
 })
 export class LandingComponent implements OnInit {
+  constructor(public auth: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  login(): void {
+    this.auth.loginWithRedirect({
+      redirect_uri: 'http://localhost:4200/tasks',
+    });
   }
 
+  ngOnInit(): void {
+    
+  }
 }
